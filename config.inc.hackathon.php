99625d266fd81ffb6a7c79b001a24720fc01df93
Pete Cinotto <pcinotto@usgs.gov>
2017-09-05 15:41:18 -0400

    Add files via upload
    Required files for 9/23/17 hackathon. 
    
    Need to add .PHP code to LowHead.hackathon.php to obtain data from the Web Service below; parse data and format for specific database requirements in USGSINKYGage2.sql database. 
    
    Indiana Department of Natural Resources 
    https://gis.in.gov/arcgis/rest/services/DNR/Indiana_LowHead_Dams/MapServer/

---
+++ config.inc.hackathon.php
@@ -0,0 +1,17 @@
+
+<?php
+
+
+
+/* Pre-define connection to the MySQL database, please specify these fields based on your database configuration.*/
+
+  define('DBHOST', 'mysql.natweb.usgs.gov');
+
+  define('DBDATA', 'USGSINKYGage2');
+
+  define('DBUSER', 'Name');
+
+  define('DBPASS', 'Password');
+
+
+?>
