45b3ee44b405faafde4ce7e59fb75baabf9c0354
Pete Cinotto <pcinotto@usgs.gov>
2017-09-05 15:44:53 -0400

    Add files via upload

---
+++ config.inc.hackathon.php
@@ -0,0 +1,17 @@
+
+<?php
+
+
+
+/* Pre-define connection to the MySQL database, please specify these fields based on your database configuration.*/
+
+  define('DBHOST', 'localhost');
+
+  define('DBDATA', 'USGSINKYGage2');
+
+  define('DBUSER', 'database_username');
+
+  define('DBPASS', 'database_password');
+
+
+?>
